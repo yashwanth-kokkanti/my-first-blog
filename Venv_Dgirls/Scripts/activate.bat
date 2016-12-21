e5535682a741b3604f7f4f02b4cf30c9e20a1762
Kokkanti <yashwanth.kokkanti@cgi.com>
2016-12-21 12:58:50 +0530

    My Django Girls app, First Commit - 1

---
+++ Venv_Dgirls/Scripts/activate.bat
@@ -0,0 +1,30 @@
+@echo off
+set "VIRTUAL_ENV=C:\D_Drive\app\PortableApps\PortableApps\Python\WinPython-64bit-3.5.2.2Qt5\notebooks\Venv_Dgirls"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+    set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+)
+set "PROMPT=(Venv_Dgirls) %PROMPT%"
+
+REM Don't use () to avoid problems with them in %PATH%
+if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+:ENDIFVHOME
+
+set PYTHONHOME=
+
+REM if defined _OLD_VIRTUAL_PATH (
+if not defined _OLD_VIRTUAL_PATH goto ENDIFVPATH1
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+:ENDIFVPATH1
+REM ) else (
+if defined _OLD_VIRTUAL_PATH goto ENDIFVPATH2
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+:ENDIFVPATH2
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
---
+++ Venv_Dgirls/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\d_drive\app\portableapps\portableapps\python\winpython-64bit-3.5.2.2qt5\notebooks\venv_dgirls\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
